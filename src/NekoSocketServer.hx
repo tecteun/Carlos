cc88630b260d6b4764207c6c3dbcc82d93a57383
tecteun <tec@tecteun.homelinux.org>
2015-12-29 14:13:10 +0100

    update

--- src/NekoSocketServer.hx
+++ src/NekoSocketServer.hx
@@ -4,9 +4,11 @@ import haxe.io.Bytes;
 import haxe.io.BytesBuffer;
 import haxe.io.BytesInput;
 import haxe.io.BytesOutput;
+import haxe.remoting.SocketConnection;
 import haxe.Utf8;
 import neko.Lib;
 import neko.net.ThreadServer;
+import sys.net.Host;
 import sys.net.Socket;
 
 #if neko
@@ -17,6 +19,7 @@ typedef Client = {
   var socket:Socket;
   var state:STATE;
   var buffer:BytesBuffer;
+  var socksocket:Socket;
 }
 
 typedef Message = {
@@ -35,6 +38,7 @@ private enum STATE {
  * https://github.com/tec27/node-argyle/blob/master/index.js
  * @see http://haxe.org/doc/neko/threadserver
  * @see http://code.google.com/p/haxe/source/browse/trunk/std/neko/net/ServerLoop.hx?r=3351
+ * http://api.haxe.org/neko/net/ServerLoop.html
  */
 class NekoSocketServer extends ThreadServer<Client, Message>
 {
@@ -50,7 +54,7 @@ class NekoSocketServer extends ThreadServer<Client, Message>
 		#if debug
 			Lib.println("client " + id + " is " + s.peer());
 		#end
-		return { socket: s, id: id, state:STATE.handshake, buffer: new BytesBuffer() };
+		return { socket: s, id: id, state: s.custom != null ? STATE.forwarding : STATE.handshake, buffer: new BytesBuffer(), socksocket: s.custom };
 	}
 
 	override function clientDisconnected( c : Client )
@@ -73,7 +77,7 @@ class NekoSocketServer extends ThreadServer<Client, Message>
 		switch(c.state) {
 			case STATE.handshake	:	retval = handleConnection(c);
 			case STATE.request		:	retval = handleRequest(c);
-			case STATE.forwarding	: null;
+			case STATE.forwarding	:	trace("fwd" + buffer.toString());  retval =  { str: buffer.toString(), bytes:buffer };  c.socksocket.output.write(buffer.sub(pos, len)); c.socksocket.output.flush();
 			
 		}
 		if (retval != null) {
@@ -128,7 +132,7 @@ class NekoSocketServer extends ThreadServer<Client, Message>
 		var addressType:Int = buffer.get(3)
 		, host:String = "undefined"
 		, port:Int = -1
-		, responseBuf;
+		, responseBuf = null;
 
 		if(addressType == 0x01) { // ipv4
 			if (buffer.length < 10) { return null; }; // 4 for host + 2 for port
@@ -136,8 +140,7 @@ class NekoSocketServer extends ThreadServer<Client, Message>
 			port = buffer.getUInt16(8);
 			responseBuf = Bytes.alloc(10);
 			responseBuf.blit(0, buffer, 0, 10);
-			buffer = buffer.sub(10, buffer.length - 10);
-			trace('host $host');
+			expandAndCopy(c, buffer.sub(10, buffer.length - 10));
 		}
 		else if (addressType == 0x03) { // dns
 			if (buffer.length < 5) { trace("no length ");  return null; };// if no length present yet
@@ -150,7 +153,7 @@ class NekoSocketServer extends ThreadServer<Client, Message>
 			responseBuf = Bytes.alloc(5 + addrLength + 2);
 			responseBuf.blit(0, buffer, 0, 5 + addrLength + 2);
 
-			buffer = buffer.sub(responseBuf.length, buffer.length - responseBuf.length );
+			expandAndCopy(c, buffer.sub(responseBuf.length, buffer.length - responseBuf.length ));
 			
 		}
 		else if(addressType == 0x04) { // ipv6
@@ -159,11 +162,26 @@ class NekoSocketServer extends ThreadServer<Client, Message>
 			trace('unsupported address type: $addressType');
 			c.socket.output.writeByte(0x05);
 			c.socket.output.writeByte(0x01);
-			//c.socket.output.flush();
-			//c.socket.close();
+			c.socket.output.flush();
+			c.socket.close();
 			return null;
 		}
-trace('Request to $host $port');
+		trace('Request to $host $port');
+		
+		var sock = new Socket();
+		sock.custom = c.socket;
+		sock.connect(new Host(host), port);
+		addSocket(sock);
+		c.socksocket = sock;
+		c.state = STATE.forwarding;
+		if(null != responseBuf){
+			responseBuf.set(1, 0);
+			responseBuf.set(2, 0);
+			c.socket.output.write(responseBuf);
+		}
+		c.socket.output.flush();
+		
+		
 		return  { str: buffer.toString(), bytes:buffer };
 		//throw "Unhandled data '"+msg+"'";
 	}
